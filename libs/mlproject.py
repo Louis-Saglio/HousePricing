c5ef02b7d3e79b05b04451f6ae42e482700aa3da
Louis-Saglio <louis.saglio@sfr.fr>
2020-10-14 15:04:40 +0200

    Add and use PropertyGetter

--- libs/mlproject.py
+++ libs/mlproject.py
@@ -9,17 +9,24 @@ class ModelInterface:
         raise NotImplementedError
 
 
-class MLProject:
+class PropertyGetter:
+    def get(self, property_name: str):
+        if hasattr(self, property_name):
+            return getattr(self, property_name)
+        elif hasattr(self, f"get_{property_name}"):
+            get_method = getattr(self, f"get_{property_name}")
+            if callable(get_method):
+                return get_method()
+        else:
+            raise NotImplementedError(f"Cannot retrieve property {property_name} of object {self}")
+
+
+class MLProject(PropertyGetter):
     columns: List[str] = None
     x_train: pd.DataFrame = None
     columns_to_dummify: List[str] = []
     bool_columns_to_integer: List[str] = []
     model: ModelInterface
-
-    def get_columns(self) -> List[str]:
-        if self.columns is not None:
-            return self.columns
-        raise NotImplementedError
 
     def load_x_train_data(self) -> pd.DataFrame:
         raise NotImplementedError
@@ -28,7 +35,7 @@ class MLProject:
         raise NotImplementedError
 
     def prepare_x_data(self):
-        self.x_train = self.load_x_train_data()[[*self.get_columns()]]
+        self.x_train = self.load_x_train_data()[[*self.get("columns")]]
         self.x_train = pd.get_dummies(self.x_train, columns=self.columns_to_dummify)
         for column_name in self.bool_columns_to_integer:
             self.x_train[column_name] = self.x_train[column_name].astype(int)
@@ -41,25 +48,18 @@ class MLProject:
         raise NotImplementedError
 
 
-
-class CSVLoaderMixin:
+class CSVLoaderMixin(PropertyGetter):
     x_train_file_path: str
     x_test_file_path: str
     y_train_file_path: str
 
     class FileType(Enum):
-        X_TRAIN = 'x_train'
-        X_TEST = 'x_test'
-        Y_TRAIN = 'y_train'
-
-    def _get_file_path(self, file_type: FileType) -> str:
-        parameter_name = f"{file_type.value}_file_path"
-        if hasattr(self, parameter_name):
-            return getattr(self, parameter_name)
-        raise NotImplementedError
+        X_TRAIN = "x_train"
+        X_TEST = "x_test"
+        Y_TRAIN = "y_train"
 
     def load_csv(self, file_type: FileType):
-        return pd.read_csv(self._get_file_path(file_type))
+        return pd.read_csv(self.get(f"{file_type.value}_file_path"))
 
     def load_x_train_data(self) -> pd.DataFrame:
         return self.load_csv(self.FileType.X_TRAIN)
