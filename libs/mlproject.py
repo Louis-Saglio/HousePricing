89396037433f98ee0c936bb283ac9867d9e5f500
Louis-Saglio <louis.saglio@sfr.fr>
2020-10-15 09:10:24 +0200

    Split DataType into two enums

--- libs/mlproject.py
+++ libs/mlproject.py
@@ -4,10 +4,14 @@ from typing import List, Optional, Dict, Any
 import pandas as pd
 
 
-class DataType(Enum):
-    X_TRAIN = "x_train"
-    X_TEST = "x_test"
-    Y_TRAIN = "y_train"
+class DataCategory(Enum):
+    TRAIN = "train"
+    TEST = "test"
+
+
+class DataInformation(Enum):
+    X = "x"
+    Y = "y"
 
 
 class ModelInterface:
@@ -29,8 +33,11 @@ class PropertyGetter:
 
 class MLProject(PropertyGetter):
     x_columns: List[str] = None
+    y_columns: List[str] = None
     x_columns_to_dummify: List[str] = []
+    y_columns_to_dummify: List[str] = []
     x_bool_columns_to_integer: List[str] = []
+    y_bool_columns_to_integer: List[str] = []
     model: ModelInterface
     fit_kwargs: Dict[str, Any]
 
@@ -41,36 +48,26 @@ class MLProject(PropertyGetter):
     @property
     def x_train(self) -> pd.DataFrame:
         if self._x_train is None:
-            self._x_train = self._prepare_data(DataType.X_TRAIN)
+            self._x_train = self._prepare_data(DataInformation.X, DataCategory.TRAIN)
         return self._x_train
 
     @property
     def y_train(self) -> pd.DataFrame:
         if self._y_train is None:
-            self._y_train = self._prepare_data(DataType.Y_TRAIN)
+            self._y_train = self._prepare_data(DataInformation.Y, DataCategory.TRAIN)
         return self._y_train
 
-    def load_x_train(self) -> pd.DataFrame:
+    def load_train_data(self) -> pd.DataFrame:
         raise NotImplementedError
 
-    def load_y_train(self) -> pd.DataFrame:
+    def load_test_data(self) -> pd.DataFrame:
         raise NotImplementedError
 
-    def _prepare_data(self, data_type: DataType) -> pd.DataFrame:
-        if data_type == DataType.X_TRAIN:
-            load_method = self.load_x_train
-            prefix = "x"
-        elif data_type == DataType.Y_TRAIN:
-            load_method = self.load_y_train
-            prefix = "y"
-        elif data_type == DataType.X_TEST:
-            load_method = self.load_x_test
-            prefix = "x"
-        else:
-            raise RuntimeError(f"Data type not recognized : {data_type}")
-        data = load_method()[[*self.get(f"{prefix}_columns")]]
-        data = pd.get_dummies(data, columns=self.get(f"{prefix}_columns_to_dummify"))
-        for column_name in self.get(f"{prefix}_bool_columns_to_integer"):
+    def _prepare_data(self, data_information: DataInformation, data_category: DataCategory) -> pd.DataFrame:
+        load_method = getattr(self, f"load_{data_category.value}_data")
+        data = load_method()[[*self.get(f"{data_information.value}_columns")]]
+        data = pd.get_dummies(data, columns=self.get(f"{data_information.value}_columns_to_dummify"))
+        for column_name in self.get(f"{data_information.value}_bool_columns_to_integer"):
             data[column_name] = data[column_name].astype(int)
         return data
 
@@ -80,16 +77,15 @@ class MLProject(PropertyGetter):
 
 
 class CSVLoaderMixin(PropertyGetter):
-    x_train_file_path: str
-    x_test_file_path: str
-    y_train_file_path: str
+    train_data_file_path: str
+    test_data_file_path: str
 
-    def load_csv(self, data_type: DataType):
-        return pd.read_csv(self.get(f"{data_type.value}_file_path"))
+    def load_csv(self, data_category: DataCategory):
+        return pd.read_csv(self.get(f"{data_category.value}_file_path"))
 
-    def load_x_train(self) -> pd.DataFrame:
+    def load_train_data(self) -> pd.DataFrame:
         # todo : use get("x_train")
-        return self.load_csv(DataType.X_TRAIN)
+        return self.load_csv(DataCategory.TRAIN)
 
-    def load_y_train(self) -> pd.DataFrame:
-        return self.load_csv(DataType.X_TEST)
+    def load_test_data(self) -> pd.DataFrame:
+        return self.load_csv(DataCategory.TEST)
